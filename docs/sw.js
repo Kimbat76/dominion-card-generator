732957493298cd5aaf88a6d033b2688822be1979
ArthurBaroni <arthur.baroni@laposte.net>
2024-08-10 17:14:24 +0200

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Names of the caches used in this version of the service worker.
 // Increase chache number when you update any of the local resources, which will in turn trigger the install event again.
-const PRECACHE_CORE = 'precache-core-v39';
+const PRECACHE_CORE = 'precache-core-v40';
 const PRECACHE_CARD = 'precache-card-v8';
 const RUNTIME = 'runtime';
 
